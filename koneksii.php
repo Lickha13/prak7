03e69f71e8c45ceaabf832d8a6967865901efb1f
Lickha13 <69150734+Lickha13@users.noreply.github.com>
2021-04-26 22:35:36 +0700

    Create koneksii.php

---
+++ koneksii.php
@@ -0,0 +1,9 @@
+<?php
+// konfigurasi database
+$host		=	"localhost";
+$user		=	"root";
+$password	=	"";
+$database	=	"prak7";
+// perintah php untuk akses ke database
+$koneksii	=	mysqli_connect($host, $user, $password, $database);
+?>
